3a2699f524c8c1d9e80d572ed5bcf1ff61a5c5fd
Nagi2880 <alexanderamaral17@gmail.com>
2022-07-13 02:54:12 -0300

    Starting the design from the mainhome and Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import styles from './components.module.css'
 function Footer() {
+  var date =  new Date()
+  var year = date.getFullYear()
   return (
     <footer id={styles.footerContainer}>
-         test
+        <span>{`©${year} All right reserved`}</span>
     </footer>
   )
 }
--- src/components/Mainhome.tsx
+++ src/components/Mainhome.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import styles from './components.module.css'
+import styles from './components.module.css';
+
 function Mainhome() {
   return (
     <main id={styles.mainContainer}>
@@ -7,10 +8,10 @@ function Mainhome() {
           <h1>Bienvenido {/* here's will go the name for the person who has logging in the game */} </h1>
           <h2>Para comenzar a jugar dale al boton que dice Iniciar</h2>
         </article>
-        <form id={styles.form} className=''>       
-          <button id={styles.buttonMainhome}> Iniciar</button>
-          <button id={styles.buttonMainhome}>Dificultad</button>
-          <button id={styles.buttonMainhome}>Logros</button>
+        <form id={styles.mainForm} className='grid col col-12'>       
+            <button className='grid col col-2' value="start" id={styles.buttonMainhome}>Iniciar</button>
+            <button className='grid col col-2' value="difficulty level" id={styles.buttonMainhome}>Dificultad</button>
+            <button className='grid col col-2' value="Achievement"id={styles.buttonMainhome}>Logros</button>
         </form>
     </main>
   )
